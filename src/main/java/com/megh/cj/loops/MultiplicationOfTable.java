66c3d90a7ada643a4a09b6a6ca42816deaa0fcd9
megha <meghanavutla96@gmail.com>
2022-09-27 21:28:23 -0500

    multiplication of programs is added

---
+++ src/main/java/com/megh/cj/loops/MultiplicationOfTable.java
@@ -0,0 +1,10 @@
+package com.megh.cj.loops;
+
+public class MultiplicationOfTable {
+    public static void main(String[] args) {
+        int num = 19;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + (num * i));
+        }
+    }
+}
